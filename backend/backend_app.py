fa8d95f9975db47fa944ea4b12d43f9ff5da79b8
Jonas Happ <github@jonashapp.com>
2024-08-10 20:35:49 +0200

    Added the Add Post Function in the Backend and Tested with Psotman. Implmented Error Handling for missing keys or input not being formatted as a dict.

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -10,9 +10,63 @@ POSTS = [
 ]
 
 
+def validate_post_data(data):
+    # Ensure data is a dictionary and has required keys
+    # is not valid = False
+    if not isinstance(data, dict):
+        return "not_a_dict", "Input is not a dict."
+
+    if "title" not in data and "content" not in data:
+        return "something_is_missing", "Title and content are required in data."
+    elif "title" not in data and "content" in data:
+        return "something_is_missing", "Title is required in data."
+    elif "content" not in data:
+        return "something_is_missing", "Content is required in data."
+
+    # is valid = True
+    return "nothing_is_missing", "good"
+
+
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
     return jsonify(POSTS)
+
+
+@app.route('/api/posts', methods=['POST'])
+def add_post():
+    if request.method == 'POST':
+        # This is the data from the body of the Post Request
+        new_post = request.get_json()
+
+        # Check if JSON was parsed successfully and contains required fields
+
+        # not yet implemented feedback on what's missing in the request:
+        is_valid, error_message = validate_post_data(new_post)
+
+        if is_valid == "something_is_missing":
+            is_valid = False
+        elif is_valid == "not_a_dict":
+            is_valid = False
+        else:
+            is_valid = True
+
+        if not new_post or not is_valid:
+            return jsonify({"error": f"Invalid post data. {error_message}"}), 400
+
+        # Generate a new ID for the book
+        # checks if there are posts already (if POSTS), otherwise gives value 1
+        # checks for the current maximum post id value and adds one as new max equals new id
+        new_id = max(post['id'] for post in POSTS) + 1 if POSTS else 1
+
+        # gives the new post dict the ID field
+        new_post['id'] = new_id
+
+        # Add the new book to our list
+        POSTS.append(new_post)
+
+        # Return the new book data to the client
+        # 201 is status code for a created object
+        return jsonify(new_post), 201
 
 
 if __name__ == '__main__':
